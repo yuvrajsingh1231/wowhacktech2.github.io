05fb5514a023b784b4cacf84999e755aded53434
yuvrajsingh1231 <50015018+yuvrajsingh1231@users.noreply.github.com>
2021-02-25 17:19:55 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,5 @@
-require('dotenv').config();
-const mongoose = require("mongoose");
 
-//const url = process.env.DATABASE_URL;
-const url = process.env.DATABASE_URL || "mongodb://localhost:27017/users 
+const url = process.env.DATABASE_URL || "mongodb://localhost:27017/users"; 
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology:true,
